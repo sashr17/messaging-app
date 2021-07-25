12975c4c8b5af6c8c3b6e9baffca21fd1cf5bb38
Santhosh R <sashr17@gmail.com>
2021-07-25 13:06:21 +0530

    code refactoring

--- server/server.js
+++ server/server.js
@@ -1,70 +1,63 @@
-const express = require("express");
-const app = express();
-const socket = require("socket.io");
-const color = require("colors");
-const cors = require("cors");
+const app = require("express")();
+const server = require("http").createServer(app);
+const options = { cors: { origin: "*" } };
+const io = require("socket.io")(server, options);
 const { getCurrentUser, removeUser, joinUser } = require("./users");
 
-app.use(express());
-
-const port = 8000;
-
-app.use(cors());
-
-var server = app.listen(
-  port,
-  console.log(`Server is running on the port no: ${port} `.green)
-);
-
-const io = socket(server);
+const PORT = process.env.PORT || 8000;
 
 //initializing the socket io connection
 io.on("connection", (socket) => {
   //for a new user joining the room
   socket.on("joinRoom", ({ userName, roomName }) => {
     //* create user
-    const p_user = joinUser(socket.id, userName, roomName);
-    console.log(socket.id, "=id");
-    socket.join(p_user.roomName);
+    const user = joinUser(socket.id, userName, roomName);
+    console.log("Socket ID >> ", socket.id);
+    socket.join(user.roomName);
 
     //display a welcome message to the user who have joined a room
     socket.emit("message", {
-      userId: p_user.id,
-      userName: p_user.userName,
-      text: `Welcome ${p_user.userName}`,
+      userId: user.id,
+      userName: user.userName,
+      text: `Welcome ${user.userName}`,
     });
 
     //displays a joined room message to all other room users except that particular user
-    socket.broadcast.to(p_user.roomName).emit("message", {
-      userId: p_user.id,
-      userName: p_user.userName,
-      text: `${p_user.userName} has joined the chat`,
+    socket.broadcast.to(user.roomName).emit("message", {
+      userId: user.id,
+      userName: user.userName,
+      text: `${user.userName} has joined the chat`,
     });
   });
 
   //user sending message
   socket.on("chat", (text) => {
     //gets the room user and the message sent
-    const p_user = getCurrentUser(socket.id);
+    const user = getCurrentUser(socket.id);
 
-    io.to(p_user.roomName).emit("message", {
-      userId: p_user.id,
-      userName: p_user.userName,
-      text: text,
-    });
+    user &&
+      io.to(user.roomName).emit("message", {
+        userId: user.id,
+        userName: user.userName,
+        text: text,
+      });
   });
 
   //when the user exits the room
   socket.on("disconnect", () => {
     //the user is deleted from array of users and a left room message displayed
-    const p_user = removeUser(socket.id);
+    const user = removeUser(socket.id);
 
-    if (p_user) {
-      io.to(p_user.roomName).emit("message", {
-        userId: p_user.id,
-        userName: p_user.userName,
-        text: `${p_user.userName} has left the room`,
+    if (user) {
+      io.to(user.roomName).emit("message", {
+        userId: user.id,
+        userName: user.userName,
+        text: `${user.userName} has left the room`,
       });
     }
   });
+});
+
+server.listen(PORT, () => {
+  console.log(`Chatcord server is running on PORT: ${PORT}`);
 });
